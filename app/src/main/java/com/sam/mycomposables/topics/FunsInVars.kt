fcab6c7298537c8de0c92cb4f09597dfec2ed03f
samuel-wakoli <swwakoli@gmail.com>
2023-06-27 15:50:00 +0300

    experimenting

---
+++ app/src/main/java/com/sam/mycomposables/topics/FunsInVars.kt
@@ -0,0 +1,36 @@
+package com.sam.mycomposables.topics
+
+fun main() {
+    val coins: (Int) -> String = {
+            size -> "$size toppings"
+    }
+
+    trickOrTreat(true, null)
+    trickOrTreat(false, extraTreat = coins)
+
+    // trickOrTreat is a higher order fun
+
+    trickOrTreat(false) {
+        "$it extra treats"
+    }
+
+}
+
+fun trickOrTreat (isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit{
+    return if (isTrick) {
+        trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(2))
+        }
+        treat
+    }
+}
+
+val trick: () -> Unit = {
+    println("No treat!")
+}
+
+val treat: () -> Unit = {
+    println("You have treats!")
+}
